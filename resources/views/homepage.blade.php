5af9adb0681615ec693c6636109a5bd3bd39ce62
Muhammad Irfan <irfantwr@gmail.com>
2024-01-11 15:22:17 +0700

    ubah

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,20 +15,23 @@
     <div class="row mx-5">
         @foreach ($sepatus as $sepatu)
             <div class="col-lg-6">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $sepatu['nama'] }}</h5>
-                                <p class="card-text" style="font-weight: 600">Rp {{ $sepatu['harga'] }},00</p>
-                                <a href="/sepatu/{{ $sepatu['id'] }}" class="btn btn-primary mt-4">Lihat Detail</a>
+                <a href="/sepatu/{{ $sepatu['id'] }}" style="text-decoration: none">
+                    <div class="box card mb-4 " style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="kol1 col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $sepatu['nama'] }}</h5>
+                                    <p class="card-text" style="font-weight: 600">Rp {{ $sepatu['harga'] }},00</p>
+                                    <p style="font-style: italic; margin-top: 28px;">Tekan untuk lihat selengkapnya...</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="/images/{{ $sepatu['foto_sampul'] }}" class="gambar img-fluid rounded-end"
+                                    alt="...">
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <img src="/images/{{ $sepatu['foto_sampul'] }}" class="img-fluid rounded-end" alt="...">
-                        </div>
                     </div>
-                </div>
+                </a>
             </div>
         @endforeach
         <div class="d-flex justify-content-center">
--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -5,6 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PanShoes - @yield('title', 'Website')</title>
+    <style type="text/css">
+    .box{
+      background-color: #ccc;
+      color: black;
+      transition: background-color 500ms 300ms, color 500ms 300ms;
+    }
+    .box:hover{
+        background-color: #000;
+        color: #ccc;
+    }
+  </style>
     <link href="/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
@@ -70,7 +81,7 @@
             <div class="container text-center">
                 <div class="row">
                     <div class="col-sm-12 mt-3">
-                        <p>&copy; copyright 2023 | built with <i class="bi bi-heart-fill"></i> by <a href="http://instagram.com/mhdirfan17_" target="_blank">Muhammad Irfan</a></p>
+                        <p>&copy; copyright 2024 | built with <i class="bi bi-heart-fill"></i> by <a href="http://instagram.com/mhdirfan17_" target="_blank">Muhammad Irfan</a></p>
                     </div>
                 </div>
             </div>
